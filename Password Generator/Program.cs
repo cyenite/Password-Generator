98bcdc94a8b0a050bcb8efe06118679b89367be8
Akron254 <aaronrono42@gmail.com>
2018-11-05 07:25:14 +0300

    Password Generator

---
+++ Password Generator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static string generatePswd(int length)
+        {
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder res = new StringBuilder();
+            Random rnd= new Random();
+            while (0 < length--)
+            {
+                res.Append(validChars[rnd.Next(validChars.Length)]);
+            }
+
+            return res.ToString();
+        }
+        static void Main(string[] args)
+        {
+            //password length
+            int length = 8;
+
+            string pass = generatePswd(length);
+            Console.WriteLine(pass);
+
+            Console.ReadLine();
+        }
+    }
+}
